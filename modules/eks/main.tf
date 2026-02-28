77b96bf648d506f251dda09d1ed4c75446cf2f9c
srushtilohakare111-hue <srushtilohakre111@gmail.com>
2026-02-28 09:47:55 +0530

    update

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -83,7 +83,10 @@ resource "aws_iam_role" "node_role" {
   }
 }
   
-
+resource "aws_iam_role_policy_attachment" "cluster_node_policy_attachment" {
+  role = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
 
 
 resource "aws_iam_policy_attachment" "node_policy_attachment" {
